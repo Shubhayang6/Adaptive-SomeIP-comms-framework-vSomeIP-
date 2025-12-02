d0193999b967d504948f9cb547c5087ba9dc690e
Shubhayan Ghosh <shubhayan.tech@gmail.com>
2025-12-02 23:15:16 +0530

    Add source files for client and service handles

---
+++ src/client.cpp
@@ -0,0 +1,66 @@
+#include <vsomeip/vsomeip.hpp>
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+#define SERVICE_ID 0x1234
+#define INSTANCE_ID 0x5678
+#define METHOD_ID 0x0421
+
+shared_ptr<vsomeip::application> app;
+
+void on_response(const shared_ptr<vsomeip::message> response)
+{
+    string res(
+        reinterpret_cast<const char *>(response->get_payload()->get_data()),
+        response->get_payload()->get_length());
+
+    cout << "[Client] Response Recieved:" << res << endl;
+}
+
+void on_state(vsomeip::state_type_e state)
+{
+    if (state == vsomeip::state_type_e::ST_REGISTERED)
+    {
+        cout << "[Client] Registered -> Sending Request..." << endl;
+
+        auto request = vsomeip::runtime::get()->create_request();
+
+        request->set_service(SERVICE_ID);
+        request->set_instance(INSTANCE_ID);
+        request->set_method(METHOD_ID);
+
+        string message = "Hello from Client";
+
+        auto payload = vsomeip::runtime::get()->create_payload();
+
+        payload->set_data(
+            reinterpret_cast<const vsomeip::byte_t *>(message.c_str()),
+            message.size());
+        request->set_payload(payload);
+
+        app->send(request, true);
+    }
+}
+
+int main()
+{
+    app = vsomeip::runtime::get()->create_application("client-sample");
+
+    if (!app->init())
+    {
+        cerr << "Failed to init client app!" << endl;
+        return 1;
+    }
+
+    app->register_state_handler(on_state);
+    app->register_message_handler(SERVICE_ID, INSTANCE_ID, METHOD_ID, on_response);
+
+    app->request_service(SERVICE_ID, INSTANCE_ID);
+
+    cout << "[Client] Waiting for service..." << endl;
+
+    app->start();
+    return 0;
+}
--- src/service.cpp
+++ src/service.cpp
@@ -4,16 +4,49 @@
 
 using namespace std;
 
-#define SERVICE_ID 0x1234
+#define SERVICE_ID  0x1234
 #define INSTANCE_ID 0x5678
-#define CLIENT_ID 0x0421
+#define METHOD_ID   0x0421
 
 shared_ptr<vsomeip::application> app;
 
-void on_response(const shared_ptr<vsomeip::message> response)
-{
-    string res(
-        reinterpret_cast<const char*>(response->get_payload()->get_data()),
-        response->get_payload()->get_length()
+void handle_request(const shared_ptr<vsomeip::message> request) {
+    string received(
+        reinterpret_cast<const char*>(request->get_payload()->get_data()),
+        request->get_payload()->get_length()
     );
+
+    cout << "[Service] Received request: " << received << endl;
+
+    auto response = vsomeip::runtime::get()->create_response(request);
+    string msg = "Hello from SOME/IP Service!";
+
+    auto payload = vsomeip::runtime::get()->create_payload();
+    payload->set_data(
+        reinterpret_cast<const vsomeip::byte_t*>(msg.c_str()),
+        msg.size()
+    );
+
+    response->set_payload(payload);
+    app->send(response);
+
+    cout << "[Service] Sent response." << endl;
+}
+
+int main() {
+    app = vsomeip::runtime::get()->create_application("service-sample");
+
+    if (!app->init()) {
+        cerr << "Failed to init service app!" << endl;
+        return 1;
+    }
+
+    app->register_message_handler(SERVICE_ID, INSTANCE_ID, METHOD_ID, handle_request);
+
+    app->offer_service(SERVICE_ID, INSTANCE_ID);
+
+    cout << "[Service] Offering service..." << endl;
+
+    app->start();
+    return 0;
 }
